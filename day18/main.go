1944733baa9a7a5c3715f1a5c99ab93e0cd25aa6
basverweij <bas@off-sync.com>
2017-12-30 21:51:44 +0100

    Completed day 18, puzzle 2.

--- day18/main.go
+++ day18/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Puzzle 1: %d\n", puzzle1())
+
+	fmt.Printf("Puzzle 2: %d\n", puzzle2())
 }
 
 func puzzle1() int {
 	return totalSafeTiles(input, 40)
+}
+
+func puzzle2() int {
+	return totalSafeTiles(input, 400000)
 }
